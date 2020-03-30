46899cfd7f1bee5b53d4a4867fcde22d5d7a6e93
Braden Mars <bradenmars@bradenmars.me>
2020-03-30 08:14:08 -0500

    feat: add handler for fetching pda/wksite by phone number

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event
-      },
-      null,
-      2
-    )
-  };
+const axios = require('axios');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+axios.defaults.baseURL = 'https://api.crisiscleanup.org';
+
+module.exports.handler = async (event, context, callback) => {
+  // Grab inbound number from event
+  const {
+    Details: {
+      Parameters: { inboundNumber },
+    },
+  } = event;
+
+  // Log
+  console.log('EVENT', event);
+  console.log('CONTEXT', context);
+
+  // Query phone outbound table
+  const queryNumber = inboundNumber.split('+')[1];
+  const response = await axios.get(
+    `/phone_outbound?phone_number=${queryNumber}`,
+    {
+      auth: {
+        username: process.env.CC_AUTH_USERNAME,
+        password: process.env.CC_AUTH_PASSWORD,
+      },
+    },
+  );
+  console.log('RESPONSE', response);
+  if (response.status !== 200) {
+    throw 'Number not found!';
+  }
+
+  const { pda, worksite } = response.data;
+  if (worksite !== null) {
+    return callback(null, { type: 'worksite', id: worksite });
+  }
+
+  if (pda !== null) {
+    return callback(null, { type: 'pda', id: pda });
+  }
+
+  throw 'Number does not have a pda or worksite associated!';
 };
