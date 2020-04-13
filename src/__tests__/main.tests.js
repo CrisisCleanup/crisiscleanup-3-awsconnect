2d454911a4c45ff88479edfbd03c59b3cfb5d64e
Braden Mars <bradenmars@bradenmars.me>
2020-04-12 21:55:06 -0500

    feat: refactor case check and add tests

--- src/__tests__/main.tests.js
+++ src/__tests__/main.tests.js
@@ -3,4 +3,35 @@
  * Main Tests
  */
 
- 
+import { Outbound } from '../api';
+import Handler from '../index';
+
+jest.mock('../api');
+
+const MockEvent = (data = {}) => ({
+  Details: {
+    Parameters: {
+      inboundNumber: '+10000000000',
+      isDev: '0',
+      action: 'CHECK_CASE',
+      userLanguage: 'en_US',
+      incidentId: '199',
+      ...data,
+    },
+  },
+});
+
+describe('handler', () => {
+  it('checks for existing case', async () => {
+    Outbound.resolveCasesByNumber = jest.fn();
+    Outbound.resolveCasesByNumber.mockResolvedValue({
+      ids: [0, 1],
+      pdas: [1],
+      worksites: [2],
+    });
+    const callback = jest.fn();
+    await Handler(MockEvent(), {}, callback);
+    expect(Outbound.resolveCasesByNumber.mock.calls.length).toBe(1);
+    expect(callback).toMatchSnapshot();
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,147 +1,47 @@
-const axios = require('axios');
+/**
+ * AWS Connect Streams Integrations
+ */
 
-const ENDPOINT = (isDev) => {
-  const ep = {
-    auth: `Token ${process.env.CC_AUTH_TOKEN}`,
-    baseUrl: process.env.CC_API_BASE_URL,
-  };
-  // AWS connect likes to cast inputs as strings
-  if (isDev === '1') {
-    (ep.auth = `Token ${process.env.CC_DEV_AUTH_TOKEN}`),
-      (ep.baseUrl = process.env.CC_DEV_API_BASE_URL);
-  }
-  return ep;
-};
+import { Outbound } from './api';
+import { configureEndpoint } from './utils';
 
-const checkCases = async (inboundNumber, callback) => {
-  // Query phone outbound table
-  const queryNumber = inboundNumber.split('+')[1];
-  const response = await axios.get(
-    `/phone_outbound?phone_number=${queryNumber}`,
-  );
-
-  // Throw error to trigger 'no ID' path in connect
-  if (response.status !== 200) {
-    console.error('response:', response);
-    throw 'Number not found!';
-  }
-
-  // Filter for outbounds w/ valid pda/worksite id
-  const { results } = response.data;
-  const cases = {
-    pdas: [],
-    worksites: [],
-    ids: [],
-  };
-
-  results.forEach(({ id, pda, worksite }) => {
-    cases.ids.push(id);
-    if (worksite !== null) {
-      return cases.worksites.push(worksite);
-    }
-    if (pda !== null) {
-      return cases.pdas.push(pda);
-    }
-  });
-  console.log('response', response);
-  console.log('cases', cases);
+const checkCases = async ({ inboundNumber }) => {
+  const cases = await Outbound.resolveCasesByNumber(inboundNumber);
 
   // Response must be simple string map
   if (cases.ids.length >= 1) {
     console.log('Case found!');
-    return callback(null, {
-      ids: cases.ids.join(','),
-      pdas: cases.pdas.join(','),
-      worksites: cases.worksites.join(','),
-    });
+    return {
+      data: {
+        ids: cases.ids.join(','),
+        pdas: cases.pdas.join(','),
+        worksites: cases.worksites.join(','),
+      },
+    };
   }
-
   // Catch all, 'no ID'
   console.log('No cases found!');
-  throw 'Number does not have a pda or worksite associated!';
+  throw new Error('Number does not have a pda or worksite associated!');
 };
 
-const getLanguageId = async (subtag) => {
-  const response = await axios.get('/languages');
-  const { results } = response.data;
-  const tag = subtag.replace('_', '-');
-  const id = results.filter((r) => (r.subtag === tag ? r.id : null));
-  if (!id.length >= 1) {
-    return 2; // en-US default
-  }
-  return id[0].id;
+const ACTIONS = {
+  CHECK_CASE: checkCases,
 };
 
-const createCallback = async (
-  inboundNumber,
-  userLanguage,
-  incidentId,
-  callback,
-) => {
-  // Request params
-  const params = {
-    dnis1: inboundNumber,
-    call_type: 'callback',
-    language: await getLanguageId(userLanguage),
-    incident_id: [incidentId],
-  };
-  console.log('creating callback...', params);
-
-  const response = await axios.post('/phone_outbound', params);
-  console.log('callback response:', response);
-  callback(null, { status: 'CREATED' });
-};
-
-const deniedCallback = async (inboundNumber, callback) => {
-  console.log('unlocking callback for:', inboundNumber);
-  // Query phone outbound table
-  const queryNumber = inboundNumber.split('+')[1];
-  const response = await axios.get(
-    `/phone_outbound?phone_number=${queryNumber}`,
-  );
-  // Throw error to trigger 'no ID' path in connect
-  if (response.status !== 200) {
-    console.error('response:', response);
-    throw 'Number not found!';
-  }
-
-  const { results } = response.data;
-  const recentId = Math.max(...results.map((o) => o.id));
-  console.log('outbound id found:', recentId);
-
-  // Unlock phone outbound
-  const resp = await axios.post(`/phone_outbound/${recentId}/unlock`);
-  if (resp.status !== 200) {
-    console.error('response:', resp);
-    throw 'Could not unlock id!';
-  }
-
-  callback(null, { status: 'UNLOCKED' });
-};
-
-module.exports.handler = async (event, context, callback) => {
+export default async (event, context, callback) => {
   // Grab inbound number from event
   const {
     Details: {
-      Parameters: { inboundNumber, isDev, action, userLanguage, incidentId },
+      Parameters: { isDev, action, ...params },
     },
   } = event;
 
-  // Get Endpoint
-  const endpoint = ENDPOINT(isDev);
-  axios.defaults.baseURL = endpoint.baseUrl;
-  axios.defaults.headers.common.Authorization = endpoint.auth;
+  // Store IS_DEV
+  process.env.IS_DEV = isDev;
+  configureEndpoint(isDev);
 
-  switch (action) {
-    case 'CHECK_CASE':
-      return checkCases(inboundNumber, callback);
-    case 'CALLBACK':
-      return createCallback(inboundNumber, userLanguage, incidentId, callback);
-    case 'DENIED_CALLBACK':
-      return deniedCallback(inboundNumber, callback);
-    default:
-      console.log('no action provided!');
-      callback(0);
-      break;
-  }
+  // Handlers
+  const { status, data } = await ACTIONS[action](params);
+  console.log('action complete. returning data:', status, data);
+  callback(status || null, data);
 };
