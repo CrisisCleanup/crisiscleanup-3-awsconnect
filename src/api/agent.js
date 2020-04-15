269ca5e0bddb44b838f4e867a51a09da60c67148
Braden Mars <bradenmars@bradenmars.me>
2020-04-14 23:50:13 -0500

    feat(agent): add methods for handling agent queue

--- src/api/agent.js
+++ src/api/agent.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /**
  * agent.js
  * Agent Api Module
@@ -14,25 +15,30 @@ export const AGENT_STATES = Object.freeze({
   OFFLINE: 'offline',
 });
 
-export const KeyMap = ({ mapName = 'Key', agentId, attributes = {} }) => ({
+export const KeyMap = ({
+  mapName = 'Key',
+  partitionKey = TABLE.hash,
+  agentId,
+  attributes = {},
+}) => ({
   [mapName]: {
-    [TABLE.hash]: {
+    [partitionKey]: {
       S: agentId,
     },
     ...attributes,
   },
 });
 
-export const setState = async ({ agentId, agentState, initContactId = '' }) => {
+export const setState = async ({ agentId, agentState, ...attrs }) => {
   const db = Dynamo.DynamoTable(TABLE);
-  let contactParams = {};
-  if (initContactId) {
-    contactParams = {
-      last_contact_id: {
-        S: initContactId,
-      },
-    };
-  }
+  const additionalAttrs = {};
+  Object.keys(attrs).forEach((key) => {
+    if (attrs[key] && attrs[key] !== null) {
+      additionalAttrs[key] = {
+        S: attrs[key],
+      };
+    }
+  });
   const params = {
     ...KeyMap({
       mapName: 'Item',
@@ -45,11 +51,70 @@ export const setState = async ({ agentId, agentState, initContactId = '' }) => {
         entered_timestamp: {
           S: new Date().toISOString(),
         },
-        ...contactParams,
+        ...additionalAttrs,
       },
     }),
   };
   console.log('setting state params:', params);
   const results = await db.putItem(params).promise();
   console.log('set agent state: ', agentId, agentState, results);
+};
+
+export const getTargetAgent = async ({ currentContactId }) => {
+  const db = Dynamo.DynamoTable(TABLE);
+  const params = {
+    IndexName: 'contact-index',
+    ExpressionAttributeValues: {
+      ':contactId': {
+        S: currentContactId,
+      },
+    },
+    KeyConditionExpression: 'current_contact_id = :contactId',
+    Select: 'ALL_ATTRIBUTES',
+  };
+  console.log('checking for target agent...', params);
+  const { Items } = await db.query(params).promise();
+  if (!Items.length) {
+    console.log('no current target agent!');
+    return null;
+  }
+  const agent = Dynamo.normalize(Items[0]);
+  console.log('got target agent!', agent);
+  return agent;
+};
+
+export const findNextAgent = async () => {
+  const db = Dynamo.DynamoTable(TABLE);
+  const params = {
+    ExpressionAttributeNames: {
+      '#S': 'state',
+    },
+    ExpressionAttributeValues: {
+      ':current': {
+        S: 'routable',
+      },
+    },
+    KeyConditionExpression: '#S = :current',
+    FilterExpression: 'attribute_not_exists(current_contact_id)',
+    IndexName: 'state-index',
+    Select: 'ALL_ATTRIBUTES',
+  };
+  console.log('querying agents table with:', params);
+  const { Items } = await db.query(params).promise();
+  const normalizedItems = Items.map((m) => Dynamo.normalize(m));
+  console.log('resulting items:', normalizedItems);
+  if (!normalizedItems.length) {
+    return null;
+  }
+  const routables = normalizedItems.filter(
+    (a) => a.state === AGENT_STATES.ROUTABLE,
+  );
+  // find the agent who's been routable the longest
+  const agent = routables.reduce((pre, cur) => {
+    return Date.parse(pre.entered_timestamp) > Date.parse(cur.entered_timestamp)
+      ? cur
+      : pre;
+  });
+  console.log('found longest standing routable agent:', agent);
+  return agent;
 };
