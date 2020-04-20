30a55a1d0be868c23561f147dffbc411474d2479
Braden Mars <bradenmars@bradenmars.me>
2020-04-20 02:41:16 -0500

    fix(config): disable all customDomains
    helps to create the route53 records, but it currently does not create mappings for websocket apis

--- config.js
+++ config.js
@@ -4,23 +4,29 @@ const DOMAINS = {
   dev: {
     domainName: 'socket.dev.crisiscleanup.io',
     stage: 'dev',
-    createRoute53Record: true,
+    createRoute53Record: false,
     endpointType: 'regional',
-    certificateName: '*.dev.crisiscleanup.io',
+    enabled: false,
+    certificateArn:
+      'arn:aws:acm:us-east-1:182237011124:certificate/7bdd9a49-4c15-47e5-803b-b9fde85614b5',
   },
   staging: {
     domainName: 'socket.staging.crisiscleanup.io',
     stage: 'staging',
-    createRoute53Record: true,
-    endpointType: 'edge',
-    certificateName: '*.staging.crisicleanup.io',
+    createRoute53Record: false,
+    endpointType: 'regional',
+    enabled: false,
+    certificateArn:
+      'arn:aws:acm:us-east-1:182237011124:certificate/fd622028-f569-43e6-83b3-6f696af0d004',
   },
   prod: {
     domainName: 'socket.crisiscleanup.org',
     stage: 'prod',
-    createRoute53Record: true,
-    endpointType: 'edge',
-    certificateName: '*.crisiscleanup.org',
+    enabled: false,
+    createRoute53Record: false,
+    endpointType: 'regional',
+    certificateArn:
+      'arn:aws:acm:us-east-1:182237011124:certificate/278f94c1-8eeb-47a6-bb9c-bb3d83a932a7',
   },
 };
 
