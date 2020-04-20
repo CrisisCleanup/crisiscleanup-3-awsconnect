4a281e60bce0658a88d680b7ac54cac96295409a
Braden Mars <bradenmars@bradenmars.me>
2020-04-20 02:40:25 -0500

    feat: add helper scripts and function upload script

---
+++ internal/scripts/deploy-functions.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+/* eslint-disable import/no-extraneous-dependencies */
+
+/*
+ * Update all function
+ *
+ */
+
+const shelljs = require('shelljs');
+
+// Vars
+const slsFunctions = ['awsConnect', 'wsHandler', 'connectionHandler'];
+const curShell = process.env.SHELL;
+const targStage = process.argv[2];
+
+if (!targStage) {
+  shelljs.echo('You must provide a stage!');
+  shelljs.exit(1);
+}
+
+const cmdBase = `sls --env ${targStage} --stage ${targStage} deploy`;
+
+shelljs.echo(`Deploying functions for: ${targStage}`);
+
+slsFunctions.forEach((func) => {
+  shelljs.echo(`- Updating: ${func}`);
+  const cmd = `${cmdBase} -f ${func} --aws-s3-accelerate`;
+  shelljs.echo(`Executing: ${cmd}`);
+  shelljs.exec(cmd, { shell: curShell });
+});
+
+shelljs.echo('Done!');
