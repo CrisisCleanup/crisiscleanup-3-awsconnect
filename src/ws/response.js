e218a48bc90673a95e58a6ab7a21901f5d117b06
Braden Mars <bradenmars@bradenmars.me>
2020-09-02 21:32:21 -0500

    feat(ws): add response module for generating consistent ws responses

---
+++ src/ws/response.js
@@ -0,0 +1,45 @@
+/**
+ * Websocket Response Helpers
+ */
+
+const buildAction = ({ namespace, actionName }) => {
+  const withData = (data) => ({
+    namespace,
+    action: {
+      type: 'action',
+      name: actionName,
+    },
+    data,
+  });
+  return withData;
+};
+
+const STREAMS = 'phone.streams';
+const CONTROLLER = 'phone.controller';
+
+const STREAMS_ACTIONS = {
+  UPDATE_AGENT: buildAction({
+    namespace: STREAMS,
+    actionName: 'updateAgentClient',
+  }),
+  UPDATE_CONTACT: buildAction({
+    namespace: STREAMS,
+    actionName: 'updateContact',
+  }),
+  UPDATE_METRICS: buildAction({
+    namespace: CONTROLLER,
+    actionName: 'updateMetrics',
+  }),
+  UPDATE_CONTACT_METRICS: buildAction({
+    namespace: CONTROLLER,
+    actionName: 'updateContactMetrics',
+  }),
+  UPDATE_AGENT_METRICS: buildAction({
+    namespace: CONTROLLER,
+    actionName: 'updateAgentMetrics',
+  }),
+};
+
+export default {
+  ...STREAMS_ACTIONS,
+};
