19033129c58ae6c9342869b134b9c57370782e3e
Braden Mars <bradenmars@bradenmars.me>
2020-09-24 13:14:49 -0500

    feat(inbound): add inbound event enum, mapping, and callback

--- src/api/inbound.js
+++ src/api/inbound.js
@@ -6,25 +6,47 @@
 import axios from 'axios';
 import { getLanguageId } from './helpers';
 
+export const InboundEvent = Object.freeze({
+  ENTER: 'enter_ivr',
+  QUEUED: 'queued',
+  ROUTED: 'routed',
+  REJECT: 'reject',
+  ABANDON: 'abandon',
+  NO_AVAILABLE: 'no_available',
+});
+
+const InboundEventMapping = {
+  PENDING: InboundEvent.QUEUED,
+  READY: InboundEvent.ROUTED,
+  NONE: InboundEvent.NO_AVAILABLE,
+  '': InboundEvent.ENTER,
+};
+
 export const create = async ({
   number,
   language,
   incidentId,
   initContactId,
   ani,
+  action = InboundEvent.ENTER,
 }) => {
   const params = {
     dnis: number,
     language: await getLanguageId(language),
     incident_id: [incidentId],
     session_id: initContactId,
-    action: 'enter_ivr',
+    action: InboundEventMapping[action] || InboundEvent.ENTER,
     ani,
   };
   console.log('creating inbound call...', params);
   const response = await axios.post('/phone_inbound', params);
   console.log('response:', response);
-  return response.data;
+  const eventCallback = async (action) => {
+    const _eventAction = action;
+    console.log('executing inbound event callback:', action, _eventAction);
+    await axios.post('/phone_inbound', { ...params, action: _eventAction });
+  };
+  return [response.data, eventCallback];
 };
 
 export const update = async (inbound, attributes) => {
